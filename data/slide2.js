0af80c1684a9d2a45ce913ec2d2e5be7d1f5a526
YOGESHKHANARA <32016427+YOGESHKHANARA@users.noreply.github.com>
2017-10-13 20:06:26 +0530

    first commit

--- data/slide2.js
+++
@@ -1,1 +0,0 @@
-(function(){var loadHandler=window['sl_{7174A014-78FD-4BEC-8239-B26702561CF0}'];loadHandler&&loadHandler(1, '<div id="spr0_e0da252"><div id="spr1_e0da252" class="kern"><img id="img0_e0da252" src="data/img1.png" width="864px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_e0da252" class="kern"></div></div>');})();
